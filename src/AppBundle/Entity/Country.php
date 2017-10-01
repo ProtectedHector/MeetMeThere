5fbcd6f4187ceed580599957d5faa6b21723eb39
Hector <ba0344@gmail.com>
2017-10-01 19:00:11 +0100

    Create Country Entity

---
+++ src/AppBundle/Entity/Country.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="country")
+ */
+class Country
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue()
+     * @ORM\OneToMany(targetEntity="Traveler", mappedBy="traveler")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=2)
+     */
+    protected $iso3166Alpha2;
+
+    /**
+     * @ORM\Column(type="string", length=3)
+     */
+    protected $iso3166Alpha3;
+
+    /**
+     * @ORM\Column(type="string", length=3)
+     */
+    protected $iso3166Numeric3;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Country
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIso3166Alpha2()
+    {
+        return $this->iso3166Alpha2;
+    }
+
+    /**
+     * @param string $iso3166Alpha2
+     *
+     * @return Country
+     */
+    public function setIso3166Alpha2($iso3166Alpha2)
+    {
+        $this->iso3166Alpha2 = $iso3166Alpha2;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIso3166Alpha3()
+    {
+        return $this->iso3166Alpha3;
+    }
+
+    /**
+     * @param string $iso3166Alpha3
+     *
+     * @return Country
+     */
+    public function setIso3166Alpha3($iso3166Alpha3)
+    {
+        $this->iso3166Alpha3 = $iso3166Alpha3;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIso3166Numeric3()
+    {
+        return $this->iso3166Numeric3;
+    }
+
+    /**
+     * @param string $iso3166Numeric3
+     *
+     * @return Country
+     */
+    public function setIso3166Numeric3($iso3166Numeric3)
+    {
+        $this->iso3166Numeric3 = $iso3166Numeric3;
+        return $this;
+    }
+
+
+}
